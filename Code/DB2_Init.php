89f91774b7ebdca0b89c2e34f8eb8ae3790d1a68
tzur <Tzur.almog@gmail.com>
2021-02-25 11:48:37 -0500

    Added DB2 Initalisation

--- Code/DB2_Init.php
+++ Code/DB2_Init.php
@@ -1,14 +1,37 @@
 <?php
 //Creates new instance of the Database that Cindy gave us
+$DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
 
 // Connects to the SQL Server
-$myconnection = mysqli_connect('localhost', 'root', '') 
-or die ('Could not connect: ' . mysql_error());
+if($myconnection = mysqli_connect('localhost', 'root', '') ){
+    echo "Connected to Sql Server<br>";
+} else  {  
+    die ('Could not connect to server');
+
+}
 
 //Checks to see if Database already exists
-if(mysqli_select_db ($myconnection, 'db2')){
-    echo "Database exists";
+if($mydb = mysqli_select_db ($myconnection, 'db2')){
+    echo "Connection made to DB2<br>";
 } else  {  
-    echo "Database doesn't exist";
+    echo "DB2 could not be found <br>";
+    if (mysqli_query($myconnection, "CREATE DATABASE DB2")) {
+        echo "DB2 created successfully";
+        $mydb = mysqli_select_db ($myconnection, 'db2');
+        } else {
+        echo "Error creating DB2: " . mysqli_error($conn);
+        die ('Could not connect to DB2');
+      }
 }
+
+//gets DB2 sql puts it into $query
+$fp = fopen("$DOCUMENT_ROOT/code/DB2.sql",'r');
+$query = "";
+while(!feof($fp)) {
+    $newLine = fgets($fp);
+    echo $newLine . "<br>";
+    $query = $query . $newLine ;
+}
+
+$result = mysqli_query($myconnection, $query)
 ?>
