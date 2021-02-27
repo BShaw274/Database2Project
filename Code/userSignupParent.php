c5920f78e44bd21f8c6e1ed5f4d2e3f517aad7c8
CGonthier424 <Connor.gonthier@gmail.com>
2021-02-27 17:44:17 -0500

    Parent Signup Complete
    Fully Finished and Commented

--- Code/userSignupParent.php
+++ Code/userSignupParent.php
@@ -1,49 +1,51 @@
 <?php
 
-$uid = "222222";
-$userEmail = "mike@tes.com";
-$userPassword ="testing123";
-$userName ="Mike-Test";
-$userPhone ="508-966-2221";
+// Getting info posted from userSignupParent.html form action line 9
+$userEmail = $_POST['userEmail'];
+$userPassword = $_POST['userPassword'];
+$userName = $_POST['userName'];
+$userPhone = $_POST['userPhone'];
 
-
-$dbConnection = new mysqli('localhost', 'root', '', 'database2');
-
+//Opening Connection to database and testing connection
+$dbConnection = new mysqli('localhost', 'root', '', 'db2');
 if ($dbConnection->connect_error) {
   die("Connection failed: " . $dbConnection->connect_error);
 }
-
+//Actual Code:
 
 // Style borrowed : https://stackoverflow.com/questions/2552545/mysqli-prepared-statements-error-reporting
-$stmt = $dbConnection->prepare("INSERT INTO users (id, email, password, name, phone) VALUES (?, ?, ?, ?, ?)");
+//Uses Prepared Statements to prepare Query String, Uses bind_param to insert variables into the Query String e
+//then pushes the query to the Database with Execute()
+$stmt = $dbConnection->prepare("INSERT INTO users (email, password, name, phone) VALUES (?, ?, ?, ?)");
 if(false ===$stmt){
   die('prepare() failed: ' . htmlspecialchars($mysqli->error));
 }
-$check = $stmt->bind_param("sssss", $uid, $userEmail, $userPassword, $userName,$userPhone);
+$check = $stmt->bind_param("ssss", $userEmail, $userPassword, $userName,$userPhone);
 if(false ===$check){
   die('bind_param() failed: ' . htmlspecialchars($stmt->error));
 }
-
 $check = $stmt->execute();
 if(false ===$check){
   die('execute() failed: ' . htmlspecialchars($stmt->error));
 }
+//Here im getting the ID of the User account previously created
+//This allows me to open another connection and push the ID into Parent/Student/Admin table as necessary
 $lastId = $stmt->insert_id;
-echo "last ID: ".$lastId."Tested|||";
-
-echo " New Records Created successfully with ID:".$uid." Email: ".$userEmail;
-
+//Outputs user info based on what was inputted, Including ID which parent must keep track of
+echo " New Records Created successfully with Info:: ID: ".$stmt->insert_id."  Email: ".$userEmail." Password: ".$userPassword." Name: ".$userName." Phone: ".$userPhone;
+//Closes stmt and connection
 $stmt->close();
 $dbConnection->close();
 
 
-// Inserting ID into parents
-$dbConnection = new mysqli('localhost', 'root', '', 'database2');
+
+//Inserting ID into parents
+//Doing the above but just the ID into parent table, parent_id
+//Connection opened and Tested
+$dbConnection = new mysqli('localhost', 'root', '', 'db2');
 if ($dbConnection->connect_error) {
   die("Connection failed: " . $dbConnection->connect_error);
 }
-//Connection opened and Tested
-
 $stmt = $dbConnection->prepare("INSERT INTO parents (parent_id) VALUES (?)");
 if(false ===$stmt){
   die('prepare() failed: ' . htmlspecialchars($mysqli->error));
@@ -60,6 +62,4 @@ if(false ===$check){
 
 $stmt->close();
 $dbConnection->close();
-
-
 ?>
