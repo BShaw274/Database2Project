003736e6b76276522845503d5cdd33cec5dfd5f1
CGonthier424 <Connor.Gonthier@gmail.com>
2021-03-13 15:35:14 -0500

    Displayed Meetings in StudentLogin.php
    Worked on by Connor G.Changed studentLogin.html to php, so i could display All meetings to students. Will need to be refined and allow Joining of meeting when that functionality is developed. Code used from ParentLogin.php

---
+++ Code/studentLogin.php
@@ -0,0 +1,84 @@
+
+
+<html>
+<head>
+  <title>Student's Homepage</title>
+</head>
+<body>
+<h1>Sunny Youth Club</h1>
+<h2>Student's  Page </h2>
+
+<h3>Update Account Information</h3>
+<p>Fill in the fields with the updated information and sumbit to update your profile! </p>
+<br>
+
+<form action="selfUpdate.php" method="post">
+
+Email:       <input type="email" id='email' name='userEmail'>
+Password:    <input type="text" id='password' name='userPassword'>
+Name:        <input type="text" id='name' name='userName'>
+Phone Number:<input type="text" id='phoneNumber' name='userPhone'>
+
+<input type="submit"><br>
+</form>
+
+<h3>Meeting Information</h3>
+<!-- Here I need to dispaly Meetings and infor for them -->
+<?php
+// Old code from ParentLogin that needs to be adapted
+//Opening Connection
+$dbConnection = new mysqli('localhost', 'root', '', 'db2');
+if ($dbConnection->connect_error) {
+  die("Connection failed: " . $dbConnection->connect_error);
+}
+//Gid stands for get id of whos ever logged in
+//session_start();
+//$gid = $_SESSION['passedId'];
+
+$stmt = $dbConnection->prepare("SELECT * from meetings");
+if(false ===$stmt){
+  die('prepare() failed: ' . htmlspecialchars($mysqli->error));
+}
+/*
+$check = $stmt->bind_param("s", $arrayOfStudents[$i]['student_id']);
+if(false ===$check){
+  die('bind_param() failed: ' . htmlspecialchars($stmt->error));
+}
+*/
+$check = $stmt->execute();
+if(false ===$check){
+  die('execute() failed: ' . htmlspecialchars($stmt->error));
+}
+
+$meetingInfoArr= $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+//var_dump($meetingInfoArr);
+
+  for($i=0; $i<10; $i++){
+  echo "Meeting # ".$i+1;
+  echo " ";
+  echo " Meet_ID: ".$meetingInfoArr[$i]['meet_id'];
+  echo " ";
+  echo " Meeting Name: ".$meetingInfoArr[$i]['meet_name'];
+  echo " ";
+  echo " Date: ".$meetingInfoArr[$i]['date'];
+  echo " ";
+  echo " Time Slot ID: ".$meetingInfoArr[$i]['time_slot_id'];
+  echo " ";
+  echo " Capacity: ".$meetingInfoArr[$i]['capacity'];
+  echo "";
+  echo " Announcement: ".$meetingInfoArr[$i]['announcement'];
+  echo "";
+  echo " Group ID: ".$meetingInfoArr[$i]['group_id'];
+  echo "";
+  ?>
+  <br>
+  <br>
+  <?php
+}
+
+
+//$stmt->close();
+
+?>
+</body>
+</html>
